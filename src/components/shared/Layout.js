d6be917293f0a32be8259d77f39996cfecc8880b
Chadwell Clark <wcclark23@comcast.net>
2021-12-19 14:40:52 -0600

    Login, signup and Not found Pages

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { useLayoutStyles } from "../../styles";
+import SEO from "./Seo.js";
+import Navbar from "./Navbar.js";
 
-function Layout() {
-  useLayoutStyles();
+function Layout({ children, title, marginTop = 60 }) {
+  const classes = useLayoutStyles();
 
-  return <div>Layout</div>;
+  return (
+    <section className={classes.section}>
+      <SEO title={title} />
+      <Navbar />
+      <main className={classes.main} style={{ marginTop }}>
+        <section className={classes.childrenWrapper}>
+          <div className={classes.children}>{children}</div>
+        </section>
+      </main>
+    </section>
+  );
 }
 
 export default Layout;
--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,7 +1,25 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
+import Layout from "../components/shared/Layout.js";
 
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <Layout title="Page Not Found" marginTop={120}>
+      <Typography variant="h5" align="center" paragraph>
+        Sorry, this page is not available.
+      </Typography>
+      <Typography variant="h5" align="center" paragraph>
+        The link you followed may be broke or the page may have been removed.
+        <Link to="/">
+          {" "}
+          <Typography color="primary" component="span">
+            Go Back to Instawell
+          </Typography>
+        </Link>
+      </Typography>
+    </Layout>
+  );
 }
 
 export default NotFoundPage;
