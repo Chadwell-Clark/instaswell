b520dd94c8ec5bdc6342c8b16a5aef0707d9e87b
Chadwell Clark <wcclark23@comcast.net>
2022-01-20 12:42:35 -0600

    Edit Profile page basics populating, not able to deploy yet due to dependecies being out of date and warnins becoming errors

---
+++ build/precache-manifest.6f61daa5f18d4be703ed38ab9036756a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40903f03a2b58e23d6bf4d56664d8d69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b639496b5f11c59e9c89",
+    "url": "/static/css/2.8e72b816.chunk.css"
+  },
+  {
+    "revision": "b639496b5f11c59e9c89",
+    "url": "/static/js/2.b72b2326.chunk.js"
+  },
+  {
+    "revision": "fb079c18f59d26ae03992a21759a0586",
+    "url": "/static/js/2.b72b2326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ce001bda1a066e15d1",
+    "url": "/static/js/3.a62d7f8f.chunk.js"
+  },
+  {
+    "revision": "09d1a68013587f0ec080",
+    "url": "/static/js/main.613c75a3.chunk.js"
+  },
+  {
+    "revision": "6482f94a4278e2f7da41",
+    "url": "/static/js/runtime-main.7652dd1e.js"
+  },
+  {
+    "revision": "cbb9b56a5cf892318fc8ab502f9d3ba7",
+    "url": "/static/media/facebook-icon-blue.cbb9b56a.svg"
+  },
+  {
+    "revision": "a9a13771e5dcd73931e0b61df2faac62",
+    "url": "/static/media/icon-sheet-2.a9a13771.png"
+  },
+  {
+    "revision": "3dfb71b4bb35ca8b35728b9475b7d861",
+    "url": "/static/media/icon-sheet.3dfb71b4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- src/pages/edit-profile.js
+++ src/pages/edit-profile.js
@@ -1,10 +1,217 @@
-import React from "react";
+import {
+  Button,
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { Menu } from "@material-ui/icons";
+import React, { useState } from "react";
+import Layout from "../components/shared/Layout";
+import ProfilePicture from "../components/shared/ProfilePicture";
+import { defaultCurrentUser } from "../data";
 import { useEditProfilePageStyles } from "../styles";
 
-function EditProfilePage() {
-  useEditProfilePageStyles();
+function EditProfilePage({ history }) {
+  const classes = useEditProfilePageStyles();
+  const path = history.location.pathname;
+  const [showDrawer, setDrawer] = useState(false);
+  const [show, set] = useState(false);
 
-  return <div>EditProfilePage</div>;
+  function handleToggleDrawer() {
+    //setDrawer(prev => !prev);
+    setDrawer(!showDrawer);
+  }
+
+  function handleSelcted(index) {
+    switch (index) {
+      case 0:
+        return path.includes("edit");
+      default:
+        break;
+    }
+  }
+
+  function handleListClick(index) {
+    switch (index) {
+      case 0:
+        history.push("/accounts/edit");
+        break;
+      default:
+        break;
+    }
+  }
+  const options = [
+    "Edit Profile",
+    "Change Profile",
+    "Apps and Websites",
+    "Email and SMS",
+    "Push Notifications",
+    "Manage Contacts",
+    "Privacy and Security",
+    "Login Activity",
+    "Emails from Instagram",
+  ];
+
+  const drawer = (
+    <List>
+      {options.map((option, index) => (
+        <ListItem
+          key={option}
+          button
+          selected={handleSelcted(index)}
+          onClick={() => handleListClick(index)}
+          classes={{
+            selected: classes.listItemSelected,
+            button: classes.listItemButton,
+          }}
+        >
+          <ListItemText primary={option} />
+        </ListItem>
+      ))}
+    </List>
+  );
+
+  return (
+    <Layout title="Edit Profile">
+      <section className={classes.section}>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          onClick={handleToggleDrawer}
+        >
+          <Menu />
+        </IconButton>
+        <nav>
+          <Hidden smUp implementation="css">
+            <Drawer
+              variant="temporary"
+              anchor="left"
+              open={showDrawer}
+              onClose={handleToggleDrawer}
+              classes={{ paperAnchorLeft: classes.temporaryDrawer }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden
+            xsDown
+            implementation="css"
+            className={classes.permanentDrawerRoot}
+          >
+            <Drawer
+              variant="permanent"
+              open
+              classes={{
+                paper: classes.permanentDrawerPaper,
+                root: classes.permanentDrawerRoot,
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+        <main>
+          {path.includes("edit") && <EditUserInfo user={defaultCurrentUser} />}
+        </main>
+      </section>
+    </Layout>
+  );
+}
+
+function EditUserInfo({ user }) {
+  const classes = useEditProfilePageStyles();
+  return (
+    <section className={classes.container}>
+      <div className={classes.pictureSectionItem}>
+        <ProfilePicture size={38} user={user} />
+        <div className={classes.justifySelfStart}>
+          <Typography className={classes.typography}>
+            {user.username}
+          </Typography>
+          <Typography
+            color="primary"
+            variant="body2"
+            className={classes.typographyChangePic}
+          >
+            Change Profile Photo
+          </Typography>
+        </div>
+      </div>
+      <form className={classes.form}>
+        <SectionItem text="Name" formItem={user.name} />
+        <SectionItem text="Username" formItem={user.username} />
+        <SectionItem text="Website" formItem={user.website} />
+        <div className={classes.sectionItem}>
+          <aside>
+            <Typography className={classes.bio}>Bio</Typography>
+          </aside>
+          <TextField
+            variant="outlined"
+            multiline
+            rowsMax={3}
+            rows={3}
+            fullWidth
+            value={user.bio}
+          />
+        </div>
+        <div className={classes.sectionItem}>
+          <div />
+          <Typography
+            color="textSecondary"
+            className={classes.justifySelfStart}
+          >
+            Personal Information
+          </Typography>
+        </div>
+        <SectionItem text="Email" formItem={user.email} type="email" />
+        <SectionItem text="Phone Number" formItem={user.phone_number} />
+        <div className={classes.sectionItem}>
+          <div />
+          <Button
+            type="submit"
+            className={classes.justifySelfStart}
+            variant="contained"
+            color="primary"
+          >
+            Submit
+          </Button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+function SectionItem({ type = "text", text, formItem }) {
+  const classes = useEditProfilePageStyles();
+  return (
+    <div className={classes.sectionItemWrapper}>
+      <aside>
+        <Hidden xsDown>
+          <Typography className={classes.typography} align="right">
+            {text}
+          </Typography>
+          <Hidden smUp>
+            <Typography className={classes.typography}>{text}</Typography>
+          </Hidden>
+        </Hidden>
+      </aside>
+      <TextField
+        variant="outlined"
+        fullWidth
+        value={formItem}
+        type={type}
+        className={classes.textField}
+        inputProps={{
+          className: classes.textFieldinput,
+        }}
+      />
+    </div>
+  );
 }
 
 export default EditProfilePage;
